1d877bea8e12ed4cb78e00268726ec57ce120a8e
Hasibul Alam Rahi <75639814+hasibulalamrahi@users.noreply.github.com>
2021-06-24 15:15:31 +0600

    Add files via upload
    It is a basic area calculator which can calculate the area of a any shape whose (area = length*breadth)

---
+++ Basic area  calculator/main.js
@@ -0,0 +1,6 @@
+function solveArea(){
+    var width = document.getElementById("width").value;
+    var length = document.getElementById("length").value;
+    var area= width*length;
+    document.getElementById("area").value = area;
+}
