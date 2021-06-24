ab9a40a74d3d4c68165e7f6c4ea6c22c0ad2cbd1
Hasibul Alam Rahi <75639814+hasibulalamrahi@users.noreply.github.com>
2021-06-24 13:15:32 +0600

    Add files via upload

---
+++ Day011/main.js
@@ -0,0 +1,16 @@
+// First Program to Print out Hello World
+console.log("Hello World");
+
+// Writing Alerts 
+alert("Hey This is an Alert ");
+// Writing on the main page using javascript 
+document.write("Hello World 2");
+
+//Writing an error in javascript 
+
+console.error("This is an Error");
+
+//writing an warning using javascript 
+
+console.warn("This is an Warning");
+
